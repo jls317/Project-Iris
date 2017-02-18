8507c4f6d6fa310aedad66862daa7d5428b2a08c
Jeffrey <Jeffrey@192.168.2.3>
2017-02-18 15:49:58 -0500

    created data pages

--- UX/public_html/myscript.js
+++ UX/public_html/myscript.js
@@ -1,29 +1,29 @@
 window.onload = datetime('trick');
-function datetime(id){
-        date = new Date;
-        year = date.getFullYear();
-        month = date.getMonth();
-        months = new Array('January', 'February', 'March', 'April', 'May', 'June', 'Jully', 'August', 'September', 'October', 'November', 'December');
-        d = date.getDate();
-        day = date.getDay();
-        days = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
-        h = date.getHours();
-        if(h<10)
-        {
-                h = "0"+h;
-        }
-        m = date.getMinutes();
-        if(m<10)
-        {
-                m = "0"+m;
-        }
-        s = date.getSeconds();
-        if(s<10)
-        {
-                s = "0"+s;
-        }
-        result = ''+days[day]+' '+months[month]+' '+d+' '+year+' '+h+':'+m+':'+s;
-        document.getElementById(id).innerHTML = result;
-        setTimeout('datetime("'+id+'");','1000');
-        return true;
+function datetime(id) {
+    date = new Date;
+    year = date.getFullYear();
+    month = date.getMonth();
+    months = new Array('January', 'February', 'March', 'April', 'May', 'June', 'Jully', 'August', 'September', 'October', 'November', 'December');
+    d = date.getDate();
+    day = date.getDay();
+    days = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
+    h = date.getHours();
+    if (h < 10)
+    {
+        h = "0" + h;
+    }
+    m = date.getMinutes();
+    if (m < 10)
+    {
+        m = "0" + m;
+    }
+    s = date.getSeconds();
+    if (s < 10)
+    {
+        s = "0" + s;
+    }
+    result = '' + days[day] + ' ' + months[month] + ' ' + d + ' ' + year + ' ' + h + ':' + m + ':' + s;
+    document.getElementById(id).innerHTML = result;
+    setTimeout('datetime("' + id + '");', '1000');
+    return true;
 }
